8c89970f5926cf6c2163a4e3c6b0949a2d41069a
W0NT0NS <62329808+W0NT0NS@users.noreply.github.com>
2021-08-18 14:26:27 +1200

    Add files via upload

---
+++ math_foundation_1.py
@@ -0,0 +1,27 @@
+##
+# File Name: math_foundation_1.py
+# Author: Nathan Wong
+# Version: 1.0.0
+# Date Created: 18/8/2021
+# Date Modifided: 18/8/2021
+
+
+# Set up maths question generator.
+
+    # Generate 2 random numbers.
+    # Print 2 numbers to users.
+    # Print sum of 2 numbers to users.
+
+# Set up maths question answer input.
+
+    # Ask user for answer input.
+    # Print correct if input = sum of question values.
+    # Print incorrect if input != sum of question values.
+
+# Set up score function.
+
+# Set up Coin flip function
+
+# Set up __main__ == __name__ function.
+
+    # Set up menu for program.

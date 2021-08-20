6da60863d903a5c379f536ce5a1c32807629bc60
W0NT0NS <62329808+W0NT0NS@users.noreply.github.com>
2021-08-20 18:12:37 +1200

    Add files via upload

---
+++ math_question_generator_2.py
@@ -0,0 +1,38 @@
+##
+# File Name: math_foundation_1.py
+# Author: Nathan Wong
+# Version: 1.0.0
+# Date Created: 18/8/2021
+# Date Modifided: 18/8/2021
+
+# Import libraries.
+import random
+
+# Set up maths question generator.
+def math_question_genrator():
+    # Generate 2 random numbers.
+    num_one = random.randrange(1,1000)
+    num_two = random.randrange(1,1000)
+    # Print 2 numbers to users.
+    print(num_one)
+    print(num_two)
+    # Print sum of 2 numbers to users.
+    print(num_one+num_two)
+
+# Set up maths question answer input.
+
+    # Ask user for answer input.
+    # Print correct if input = sum of question values.
+    # Print incorrect if input != sum of question values.
+
+# Set up score function.
+
+# Set up Coin flip function
+    
+    # Flip a coin
+
+# Set up __main__ == __name__ function.
+while __name__ == "__main__":
+    math_question_genrator()
+    break
+    # Set up menu for program.
